b897db6af114eac27393f34699d9ccd7124a4c8a
July Moss <july.x.moss@gmail.com>
2021-12-19 22:25:23 +0000

    updated naming pets part

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -1,7 +1,10 @@
 const Pet = require('../pet.js');
 
 describe('constructor', () => {
-  it('returns an object', () => {
-    expect(new Pet('Fido')).toBeInstanceOf(Object);
+
+  it('sets the name property', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.name).toEqual('Fido');
   });
 });
