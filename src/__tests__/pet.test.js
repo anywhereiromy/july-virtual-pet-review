ba379ec427c6f27cc4178c515a9a64f50d21eb75
July Moss <july.x.moss@gmail.com>
2021-12-19 22:38:44 +0000

    udpated getting older and unhealthier

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -8,3 +8,15 @@ describe('constructor', () => {
     expect(pet.name).toEqual('Fido');
   });
 });
+
+describe('growUp', () => {
+  it('increments the age by 1 and the hunger by 5, decrements the fitness by 3', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+    expect(pet.hunger).toEqual(5);
+    expect(pet.fitness).toEqual(7);
+  });
+});
