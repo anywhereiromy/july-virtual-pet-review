640de6c95b67ea0f5be3f1be330523a55c0104ef
July Moss <july.x.moss@gmail.com>
2021-12-24 09:21:22 +0000

    updated get unhealthier

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -26,5 +26,21 @@ describe ('constructor/prototype', () => {
      pet.growUp();
      //assertion
      expect (pet.age).toEqual (1);
-   })
+   });
+
+   test ('increments the hunger by 5', () => {
+    //act
+    const pet = new Pet('Fido');
+    pet.growUp();
+    //assertion
+    expect (pet.hunger).toEqual (5);
+  });
+
+  test ('decrements the fitness by 3', () => {
+    //act
+    const pet = new Pet('Fido');
+    pet.growUp();
+    //assertion
+    expect (pet.fitness).toEqual (7);
+  });
 });
