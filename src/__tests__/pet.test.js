16a3bd28c4b201f20b2a12132b46c59bfd312df0
July Moss <july.x.moss@gmail.com>
2021-12-24 09:13:30 +0000

    updated naming pets

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -1,6 +1,6 @@
 const { Pet } = require ('../pet.js');
 
-describe ('constructor', () => {
+describe ('constructor/prototype', () => {
 
   test ('returns an object', () => {
     expect (new Pet('Fido')).toBeInstanceOf(Object);
@@ -12,4 +12,19 @@ describe ('constructor', () => {
     //assertion
     expect (pet.name).toEqual ('Fido');
   });
+
+  test ('has an initial age of 0', () => {
+    //act
+    const pet = new Pet('Fido');
+    //assertion
+    expect (pet.age).toEqual (0);
+   });
+
+   test ('increments the age by 1', () => {
+     //act
+     const pet = new Pet('Fido');
+     pet.growUp();
+     //assertion
+     expect (pet.age).toEqual (1);
+   })
 });
--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,11 @@
 
 function Pet (name) {
     this.name = name;
+    this.age = 0;
+}
+
+Pet.prototype.growUp = function () {
+        this.age ++;
 }
 
 module.exports = { Pet };
