43d4cd7dc608e1245543c62a9be23181ddd6ea48
July Moss <july.x.moss@gmail.com>
2021-12-25 13:47:21 +0000

    updated walk and feed funtions

--- src/__tests__/pet.test.js
+++ src/__tests__/pet.test.js
@@ -21,26 +21,49 @@ describe ('constructor/prototype', () => {
    });
 
    test ('increments the age by 1', () => {
-     //act
+     //arange
      const pet = new Pet('Fido');
+     //act
      pet.growUp();
      //assertion
      expect (pet.age).toEqual (1);
    });
 
    test ('increments the hunger by 5', () => {
-    //act
+    //arrange
     const pet = new Pet('Fido');
+    //act
     pet.growUp();
     //assertion
     expect (pet.hunger).toEqual (5);
   });
 
   test ('decrements the fitness by 3', () => {
-    //act
+    //arrange
     const pet = new Pet('Fido');
+    //act
     pet.growUp();
     //assertion
     expect (pet.fitness).toEqual (7);
   });
+
+  test ('increments the fitness by 4', () => {
+    //arrange
+    const pet = new Pet('Fido');
+    pet.fitness = 8;
+    //act
+    pet.walk();
+    //assertion
+    expect (pet.fitness).toEqual (10);
+  })
+
+  test ('decrements the hunger by 3', () => {
+    //arrange
+    const pet = new Pet('Fido');
+    pet.hunger = 2;
+    //act
+    pet.feed();
+    //assertion
+    expect (pet.hunger).toEqual (0);
+  })
 });
--- src/pet.js
+++ src/pet.js
@@ -1,4 +1,8 @@
 
+const MAXIMUM_FITNESS = 10;
+const MINIMUM_HUNGER = 0;
+
+
 function Pet (name) {
     this.name = name;
     this.age = 0;
@@ -10,6 +14,22 @@ Pet.prototype.growUp = function () {
         this.age ++;
         this.hunger += 5;
         this.fitness -= 3;
+}
+
+Pet.prototype.walk = function () {
+    if ((this.fitness + 4) <= MAXIMUM_FITNESS) {
+        this.fitness += 4;
+    } else {
+        this.fitness = MAXIMUM_FITNESS;
+    };
+}
+
+Pet.prototype.feed = function () {
+    if ((this.hunger - 3) >= MINIMUM_HUNGER) {
+        this.hunger -= 3;
+    } else {
+        this.hunger = MINIMUM_HUNGER;
+    };
 }
 
 module.exports = { Pet };
